99a1d0e4490e1ffe80422a6585f0abdfafb15feb
sbouadi <sarahbouadi0508@gmail.com>
2021-11-21 18:43:26 +0100

    mapRead

--- Malloc_World_-_Freeing_of_the_dedicated_ram/sources/save.c
+++ Malloc_World_-_Freeing_of_the_dedicated_ram/sources/save.c
@@ -33,17 +33,70 @@ void freeMap(int** map){
     map = NULL;
 }
 
+
+void writeInMapRead(int** mapRead, int* saveInt, int row){
+    for (size_t i=0; i<4; i+=1) {
+        mapRead[row][i] = saveInt[i];
+    }
+    printf("\n");
+}
+
+int** readFile(){
+    char string[255] ="";
+    int* saveInt =NULL;
+
+    FILE* f = fopen("save.txt", "r");
+    int c=0;
+    int row=0;
+
+    int** mapRead=malloc(sizeof(int*)*4);
+
+    for (size_t i=0; i<4; i+=1) {
+        mapRead[i] = malloc(sizeof(int)*4);
+    }
+
+    for (size_t i =0; i<4; i+=1) {
+        for (size_t j =0; j <4; j+=1) {
+            mapRead[i][j] = 0;
+        }
+    }
+
+    displayMap(mapRead);
+
+    while(fgets(string, 255, f) != NULL){
+        char* stringTok = strtok(string," ");
+        saveInt = malloc(sizeof(int) * 10);
+        c= 0;
+
+        while (stringTok != NULL) {
+            saveInt[c]= atoi(stringTok);
+            stringTok= strtok(NULL, " ");
+            c+=1;
+        }
+
+        writeInMapRead(mapRead, saveInt, row);
+        row += 1;
+    }
+
+    displayMap(mapRead);
+
+    fclose(f);
+    freeMap(mapRead);
+    return mapRead;
+}
+
 int main(int argc, char const *argv[]) {
-    int** map = malloc(sizeof(int*) * 4);
+    int** map = malloc(sizeof(int*)*4);
     for (size_t i =0; i < 4; i+=1) {
-        map[i] = malloc(sizeof(int) * 4);
+        map[i] = malloc(sizeof(int)*4);
     }
     for (size_t i = 0; i < 4; i+=1) {
         for (size_t j = 0; j < 4; j+=1) {
-            map[i][j] = 0;
+            map[i][j] = 1;
         }
     }
-    displayMap(map);
     writeInFile(map);
+    readFile();
+    freeMap(map);
     return 0;
 }
